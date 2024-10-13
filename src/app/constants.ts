60c56c3d584597eb2477e46517933ea8843249f8
hernan527 <39295480+hernan527@users.noreply.github.com>
2024-10-13 09:37:03 -0300

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://servidor-queplan.onrender.com';
+export const SERVER_URL = 'https://salud.tuchat.com.ar';
 // export const SERVER_URL = 'http://localhost:5200';
 
 
